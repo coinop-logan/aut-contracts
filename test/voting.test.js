bc2476616f7db0e4d52cec8c56b14227460ecfdf
coinop-logan <coinop.logan@gmail.com>
2023-06-22 17:12:14 -0800

    Remove all other tests

---
+++ test/voting.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+describe("AutID", function () {
+    describe("mint", function () {
+        it("BLA BLA BLA BLA BLA", async function () {
+            expect(3).to.eq(3);
+        })
+    })
+})
